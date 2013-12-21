23f7b1080b81f712c6bc1d0f01b1297b4ee07a19
jalan1piyush <jalan1piyush@gmail.com>
2013-12-21 16:08:19 +0100

    Update Drawing.java

--- Drawing.java
+++ Drawing.java
@@ -1,37 +1,36 @@
-
 import java.awt.*;
 import java.awt.event.*;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import java.awt.geom.*;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.sql.*;
 
 public class Drawing extends JPanel implements MouseListener, MouseMotionListener, KeyListener{
 		
 	private static final long serialVersionUID = 1L;
-	public int l, x, y, x1, y1, x2, y2, mx, my, xc, yc, del, alton, altoff;
-	public double startx, starty, endx, endy;
-	public boolean alts;
-	public ArrayList<ArrayList<Double>> coord;
+	private int l, x, y, x1, y1, x2, y2, mx, my, xc, yc, del, alton, altoff;
+	private boolean alts;
+	private ArrayList<ArrayList<Double>> coord;
 	public ArrayList<Integer> selection;
-	public ArrayList<Shape> coords;
-	public String Mode;
-	public Point2D.Double t1, t2, center;
-	public Ellipse2D.Double p1;
-	public Line2D.Double l1;
-	public Rectangle2D.Double r1;
-	public Cursor move, def, tar, hand;
-	public Shape shp, shpm;
-        static final String USER = "root";
-        static final String PASS = "0000";
-
+	public static ArrayList<Shape> coords;
+	//private double[] id, lop, startx, starty, endx, endy;
+	private String Mode;
+	public String sfile;
+	private Point2D.Double t1, t2;
+	private Ellipse2D.Double p1;
+	private Line2D.Double l1;
+	private Rectangle2D.Double r1;
+	private Cursor move, def, tar, hand;
+	private Shape shp, shpm;
+	private Color red, green, blue , black, orange, yellow, purple;
 	
 	// Create drawing surface and introduce Mouse Listeners 
 	// for motion and click events
 	public Drawing() {
+		setDoubleBuffered(true);
 		selection = new ArrayList<Integer>();
 		coords = new ArrayList<Shape>();
 		coord = new ArrayList<ArrayList<Double>>();
@@ -40,6 +39,7 @@ public class Drawing extends JPanel implements MouseListener, MouseMotionListene
 			co = new ArrayList<Double>();
 			coord.add(co);
 		}
+		setSfile(new String (""));
 		setFocusable(true);
 		requestFocusInWindow();
 		addMouseListener(this);
@@ -49,14 +49,19 @@ public class Drawing extends JPanel implements MouseListener, MouseMotionListene
 		hand = Cursor.getPredefinedCursor(Cursor.HAND_CURSOR);
 		move = Cursor.getPredefinedCursor(Cursor.MOVE_CURSOR);
 		tar = Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR);
-		DrawShapes(getGraphics());
+		green = new Color(0,150,0);
+		blue = new Color(0,0,255);
+		purple = new Color(128,0,128);
+		red = new Color (255,0,0);
+		yellow = new Color (255,255,0);
+		orange = new Color (255,69,0);
+		black = new Color (0,0,0);
 	}
 	
 	//Get Mode set GUIFrame MenuItem
 	public void set(String m){
 		Mode = m;
 		System.out.println("Mode:"+Mode);
-		DrawShapes(getGraphics());
 	}
 	
 	// Draw Items from coords ArrayList
@@ -74,17 +79,16 @@ public class Drawing extends JPanel implements MouseListener, MouseMotionListene
 				}
 			}
 		}
-	}
+		
+}
 	
 	// Draw Point as a filled Ellipse and 
 	// add it to coords ArrayList after mouse click
-	public void DrawPoint(Graphics g, double x, double y){
-		Graphics2D shape = (Graphics2D) g;
+	public void DrawPoint(double x, double y){
+		//Graphics2D shape = (Graphics2D) g;
 		p1 = new Ellipse2D.Double(x-3, y-3, 6, 6);
 		coords.add(p1);
-		shape.fill(p1);
 		DrawShapes(getGraphics());
-		System.out.println(coords);
 	}
 	
 	// Draw itermediate lines, single line after 2 separate mouse clicks,  
@@ -94,12 +98,13 @@ public class Drawing extends JPanel implements MouseListener, MouseMotionListene
 		l1 = new Line2D.Double(x1, y1, x2, y2);
 		if (a==true){coords.add(l1);}
 		shape.draw(l1);
+		DrawShapes(getGraphics());
 	}
 	
 	// Show selected items
 	public void DrawSel(Graphics g, ArrayList<Integer> sel){
 		Graphics2D shape = (Graphics2D) g;
-		shape.setColor(Color.red);
+		shape.setColor(red);
 		// Differentiation between points to draw
 		for (int i = 0; i<sel.size(); i++)
 			if (coords.get(sel.get(i)).getClass().equals(Ellipse2D.Double.class)) {
@@ -117,15 +122,20 @@ public class Drawing extends JPanel implements MouseListener, MouseMotionListene
 	// Creation of visual Bounding Box
 	public void DrawSelBox(Graphics g, int x1, int y1, int x2, int y2){
 		Graphics2D shape = (Graphics2D) g;
-		shape.setColor(Color.blue);
-		shape.setStroke(new BasicStroke(4));
+		shape.setStroke(new BasicStroke(3));
 		x = Math.min(x1, x2);  
 		y = Math.min(y1, y2);
 		int abx = Math.abs(x1 - x2);  int aby = Math.abs(y1 - y2);
 		r1 = new Rectangle2D.Double(x, y, abx, aby);
-		//shape.setColor(Color.TRANSLUCENT);
+		
+		if(x1<x2){
+			shape.setColor(blue);
+		} else {
+			shape.setColor(green);
+			shape.setStroke(new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 10.0f, new float[]{9}, 2.0f));
+		}
 		DrawShapes(getGraphics());
-		shape.drawRect(x, y, abx, aby);
+		shape.drawRect(x, y, abx, aby);	
 		for (int i = 0; i < coords.size(); i++){
 			if (x1 > x2 && coords.get(i).intersects(r1) && selection.contains(i)==false){
 				selection.add(i);
@@ -133,6 +143,7 @@ public class Drawing extends JPanel implements MouseListener, MouseMotionListene
 			else if (x1 < x2 && r1.contains(coords.get(i).getBounds()) && selection.contains(i)==false) {
 				selection.add(i);		
 			}
+			
 			DrawSel(getGraphics(), selection);
 		}
 	}
@@ -158,10 +169,10 @@ public class Drawing extends JPanel implements MouseListener, MouseMotionListene
 			// Differentiation between points to draw
 			if (coords.get(sel.get(i)).getClass().equals(Ellipse2D.Double.class)) {
 				if(a==2){
-					shape.setColor(Color.red);
+					shape.setColor(red);
 					p1 = new Ellipse2D.Double(x-4, y-4, 8, 8);
 				} else if(a==3){
-					shape.setColor(Color.black);
+					shape.setColor(black);
 					coords.remove((int)sel.get(i));
 					p1 = new Ellipse2D.Double(x-3, y-3, 6, 6);
 					coords.add(p1);
@@ -171,22 +182,23 @@ public class Drawing extends JPanel implements MouseListener, MouseMotionListene
 				shape.fill(p1);
 			// And of lines to draw
 			} else {
+				if(a==1){
+					xc = (int)x;
+					yc = (int)y;
+					l1 = ((Line2D.Double) coords.get((int)sel.get(i)));
+					shape.setColor(red);
+				}
 				if(a==2){
-					shape.setColor(Color.red);
-					xc = (int)(((((Line2D) coords.get((int)sel.get(i))).getX1()+((Line2D) coords.get((int)sel.get(i))).getX2())/2)-x);
-					yc = (int)(((((Line2D) coords.get((int)sel.get(i))).getY1()+((Line2D) coords.get((int)sel.get(i))).getY2())/2)-y);
-					t1 = new Point2D.Double(((Line2D) coords.get((int)sel.get(i))).getX1()-xc,((Line2D) coords.get((int)sel.get(i))).getY1()-yc);
-					t2 = new Point2D.Double(((Line2D) coords.get((int)sel.get(i))).getX2()-xc,((Line2D) coords.get((int)sel.get(i))).getY2()-yc);
-					l1 = new Line2D.Double(t1, t2);				
+					shape.setColor(red);
+					t1 = new Point2D.Double(((Line2D) coords.get((int)sel.get(i))).getX1()+(x-xc),((Line2D) coords.get((int)sel.get(i))).getY1()+(y-yc));
+					t2 = new Point2D.Double(((Line2D) coords.get((int)sel.get(i))).getX2()+(x-xc),((Line2D) coords.get((int)sel.get(i))).getY2()+(y-yc));
+					l1 = new Line2D.Double(t1, t2);
 				} else if(a==3){
-					shape.setColor(Color.black);
-					xc = (int)(((((Line2D) coords.get((int)sel.get(i))).getX1()+((Line2D) coords.get((int)sel.get(i))).getX2())/2)-x);
-					yc = (int)(((((Line2D) coords.get((int)sel.get(i))).getY1()+((Line2D) coords.get((int)sel.get(i))).getY2())/2)-y);
-					t1 = new Point2D.Double(((Line2D) coords.get((int)sel.get(i))).getX1()-xc,((Line2D) coords.get((int)sel.get(i))).getY1()-yc);
-					t2 = new Point2D.Double(((Line2D) coords.get((int)sel.get(i))).getX2()-xc,((Line2D) coords.get((int)sel.get(i))).getY2()-yc);
-					l1 = new Line2D.Double(t1, t2);						
-					coords.add(l1);
-					coords.remove((int)sel.get(i));
+					shape.setColor(black);
+					t1 = new Point2D.Double(((Line2D) coords.get((int)sel.get(i))).getX1()+(x-xc),((Line2D) coords.get((int)sel.get(i))).getY1()+(y-yc));
+					t2 = new Point2D.Double(((Line2D) coords.get((int)sel.get(i))).getX2()+(x-xc),((Line2D) coords.get((int)sel.get(i))).getY2()+(y-yc));
+					((Line2D) coords.get((int)sel.get(i))).setLine(t1, t2);
+					//shape.draw(l1);
 					selection.clear();
 				}
 				shape.setStroke(new BasicStroke(5));
@@ -196,130 +208,70 @@ public class Drawing extends JPanel implements MouseListener, MouseMotionListene
 		}
 	}
 		
-	
-	public void Save(){
-		//coord.clear();
+	// Export of coords into Double ArrayLists
+	public void Save(boolean s){
+		
 		for (int i = 0; i < coords.size(); i ++) {
 			if (coords.get(i).getClass().equals(Ellipse2D.Double.class)) {
-				startx = ((Ellipse2D) coords.get(i)).getX();
-				starty = ((Ellipse2D) coords.get(i)).getY();
 				coord.get(0).add((double)i);
 				coord.get(1).add((double)0);
-				coord.get(2).add(startx);
-				coord.get(3).add(starty);
+				coord.get(2).add(((Ellipse2D) coords.get(i)).getX());
+				coord.get(3).add(((Ellipse2D) coords.get(i)).getY());
 				coord.get(4).add((double) 0);
 				coord.get(5).add((double) 0);
 			} else if ((coords.get(i).getClass().equals(Line2D.Double.class))){
-				startx = ((Line2D) coords.get(i)).getX1();
-				starty = ((Line2D) coords.get(i)).getY1();
-				endx = ((Line2D) coords.get(i)).getX2();
-				endy = ((Line2D) coords.get(i)).getY2();
 				coord.get(0).add((double)i);
 				coord.get(1).add((double)1);
-				coord.get(2).add(startx);
-				coord.get(3).add(starty);
-				coord.get(4).add(endx);
-				coord.get(5).add(endy);
+				coord.get(2).add(((Line2D) coords.get(i)).getX1());
+				coord.get(3).add(((Line2D) coords.get(i)).getY1());
+				coord.get(4).add(((Line2D) coords.get(i)).getX2());
+				coord.get(5).add(((Line2D) coords.get(i)).getX2());
 			}
+		} 
+		if (s==false){
+			JFileChooser saveFile = new JFileChooser();
+			saveFile.setMultiSelectionEnabled(false);
+			saveFile.setFileFilter(new FileNameExtensionFilter("csv", "CSV Format"));
+                        saveFile.setDialogTitle("Save");
+                        saveFile.setApproveButtonText("Save");
+        	saveFile.showOpenDialog(null);
+        	String file = saveFile.getSelectedFile().getAbsolutePath();
+        	sfile = file.replace("\\", "/");
+        	System.out.println(sfile);
+        	//sfile = file;
 		}
-		System.out.println(coord);
-               // GUIFrame.coords(coord);                
-                
-                
-                
-                
-                
-                
-                 Connection conn = null;
-                 Statement stmt = null;
-                 try{
-                     
-                 // JDBC driver name and database URL
-                     
-                String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-                String DB_URL = "jdbc:mysql://localhost/LANDP";
-                
-                 //STEP 2: Register JDBC driver
-                 Class.forName("com.mysql.jdbc.Driver");
+		Updatenow.Save(coord, sfile);
+		//System.out.println(coord);
+		for (int c = 0; c<coord.size();c++){
+			coord.get(c).clear();
+		}
+	}
+		
+	
+	// Open CSV file and Import from Database
+	public void Open(){
 
-                 //STEP 3: Open a connection
-                 System.out.println("Connecting to database...");
-                 conn = DriverManager.getConnection(DB_URL, USER, PASS);
-                 stmt = conn.createStatement();
-                                String sqlinsert = ""; 
-                                String a1 = String.valueOf(coord.get(0).get(0));
-                                System.out.println(a1);
-                                System.out.println(coord.get(0).get(0));
-          
-                                
-                         
-                                for (int i=0; i<coord.get(0).size();i++)
-        {
-            sqlinsert = "INSERT INTO COORDINATES (ID,LOP,STARTX,STARTY,ENDX,ENDY)"+
-                            "VALUES ("+  
-                            coord.get(0).get(i)+", "+
-                            coord.get(1).get(i)+", "+
-                            coord.get(2).get(i)+", "+
-                            coord.get(3).get(i)+", "+
-                            coord.get(4).get(i)+", "+
-                            coord.get(5).get(i)+")";
-                            stmt.executeUpdate(sqlinsert);
-                
-        };
-        
-        
-                              /*
-            sqlinsert = "INSERT INTO COORDINATES (ID,LOP,STARTX,STARTY,ENDX,ENDY)"+
-                            "VALUES (0,0,1.0,1.0,1.0,1.0);";
-                            System.out.println("vhj: "+sqlinsert);
-                            stmt.executeUpdate(sqlinsert);
-                */
-        
-        
-                      
-                 
-                 
-                 
-                 
-                 
-
-               
-                 }
-                 catch(SQLException se){
-                  //Handle errors for JDBC
-                   se.printStackTrace();
-                 }
-                 catch(Exception e){
-                    //Handle errors for Class.forName
-                    e.printStackTrace();
-                 }
-                 finally{
-                    //finally block used to close resources
-                    try{
-                       if(stmt!=null)conn.close();
-                    }
-                    catch(SQLException se){
-                    }// do nothing
-                    try{
-                       if(conn!=null)conn.close();
-                    }
-                    catch(SQLException se){
-                       se.printStackTrace();
-                    }//end finally try
-                  }//end try
-            System.out.println("Goodbye!");
-                       
+		
+		JFileChooser openFile = new JFileChooser();
+        	openFile.showOpenDialog(null);
+        	String ofile = openFile.getSelectedFile().getAbsolutePath();
+                ofile = ofile.replace("\\", "/");
+        	System.out.println(ofile);
+        	//sfile = file;
+                Updatenow.Open(ofile);
+		DrawShapes(getGraphics());
+		
 	}
 	
 	// MousePressed listener for mouse click events
 	public void mousePressed (MouseEvent event) {
+		x = event.getX();
+		y = event.getY();
 		//Left-Click events
 		if (SwingUtilities.isLeftMouseButton(event)){
 			// Get X, Y for point - Initiate DrawPoint()
 			if (Mode == "point"){
-				x = event.getX();
-				y = event.getY();
-				DrawPoint(getGraphics(), x, y);
+				DrawPoint(x, y);
 	    	}
 			// Get X1, Y1, X2, Y2 for line - Initiate DrawLine()
 	    	if (Mode == "line"){
@@ -365,6 +317,7 @@ public class Drawing extends JPanel implements MouseListener, MouseMotionListene
 	    				selection.add(i);
 	    				DrawShapes(getGraphics());
 	    				DrawSel(getGraphics(), selection);
+	    				DrawMove(getGraphics(), x, y, 1, selection);
 	    			}
 				}
 			}
@@ -458,4 +411,13 @@ public class Drawing extends JPanel implements MouseListener, MouseMotionListene
 	public void mouseExited(MouseEvent event) {}
 	public void mouseClicked(MouseEvent event) {}
 
+	public String getSfile() {
+		return sfile;
+	}
+
+	public void setSfile(String sfile) {
+		this.sfile = sfile;
+	}
+
 }
+	
